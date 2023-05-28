05fdd163adace350f4ee2742f80013f9a7c3ac49
dhamithdev <dhamith@bluebaylabs.co>
2023-05-29 00:12:52 +0530

    login and signup

--- components/Products.js
+++ components/Products.js
@@ -1,36 +1,65 @@
 import React from 'react'
-import { View, StyleSheet , FlatList , Image} from 'react-native';
+import { View, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native';
 import { Card } from '@rneui/themed';
 
-const Products = () => {
+const Products = ({navigation}) => {
 
     return (
         <View style={styles.container}>
             <FlatList
                 data={[
-                    { key: '1' , name : 'Frash Fruits & Vegetable', image : 'https://images.pexels.com/photos/3025236/pexels-photo-3025236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-                    { key: '2' , name : 'Cooking Oil & Ghee', image : 'https://images.pexels.com/photos/8469439/pexels-photo-8469439.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
-                    { key: '3' , name : 'Meat & Fish', image : 'https://images.pexels.com/photos/7451973/pexels-photo-7451973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
-                    { key: '4' , name : 'Bakery & Snacks', image :'https://images.pexels.com/photos/6550776/pexels-photo-6550776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
-                    { key: '5' , name : 'Dairy & Eggs', image : 'https://images.pexels.com/photos/2959317/pexels-photo-2959317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}, 
-                    { key: '6' ,name : 'Beverages', image : 'https://images.pexels.com/photos/2531184/pexels-photo-2531184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+                    {
+                        key: '1', name: 'Frash Fruits & Vegetable', image: 'https://images.pexels.com/photos/3025236/pexels-photo-3025236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                        color: 'rgba(83, 177, 117, 0.3)'
+                    },
+                    {
+                        key: '2', name: 'Cooking Oil & Ghee', image: 'https://images.pexels.com/photos/8469439/pexels-photo-8469439.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                        color: '    rgba(248, 164, 76, 0.3)'
+                    },
+                    {
+                        key: '3', name: 'Meat & Fish', image: 'https://images.pexels.com/photos/7451973/pexels-photo-7451973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                        color: '   rgba(211, 176, 224, 0.3)'
+                    },
+                    {
+                        key: '4', name: 'Bakery & Snacks', image: 'https://images.pexels.com/photos/6550776/pexels-photo-6550776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                        color: '   rgba(253, 229, 152, 0.3)'
+                    },
+                    {
+                        key: '5', name: 'Dairy & Eggs', image: 'https://images.pexels.com/photos/2959317/pexels-photo-2959317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                        color: '   rgba(247, 165, 147, 0.3)'
+                    },
+                    {
+                        key: '6', name: 'Beverages', image: 'https://images.pexels.com/photos/2531184/pexels-photo-2531184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                        color: '   rgba(183, 223, 245, 0.3)'
+                    },
                 ]}
-                renderItem={({item}) =>  
-                    <Card key={item.key}>
-                        <Card.Title>{item.name}</Card.Title>
-                        <Card.Image  
-                source={{ uri: item.image }} />
-                    </Card> }
-                />
+
+                renderItem={({ item }) =>
+                    <TouchableOpacity key={item.key} >
+                        <Card containerStyle={{
+                            borderColor : `${item.color} 5 solid` ,
+                            borderRadius: 20,
+                        }}>
+
+                            <Card.Title>{item.name}</Card.Title>
+                            <Card.Divider />
+                            <Card.Image style={{ borderRadius: 20, }}
+                                source={{ uri: item.image }} />
+
+                        </Card>
+
+                    </TouchableOpacity>
+                }
+            />
 
         </View>
     )
 }
 
+
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+   
 })
 
 export default Products
--- components/RegisterScreen.js
+++ components/RegisterScreen.js
@@ -1,34 +1,73 @@
-import React from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
 
-const RegisterScreen = () => {
+import {getAuth, createUserWithEmailAndPassword , signWithEmailAndPassword} from 'firebase/auth'
 
-    const handleLogin = () => {
-        // Handle login logic here
-        navigation.navigate('RegisterScreen');
-    };
+import { initializeApp } from 'firebase/app';
 
-    return (
-        <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                placeholder="Usernames"
-                autoCapitalize="none"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Mobile number"
-                autoCapitalize="none"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry
-                autoCapitalize="none"
-            />
-            <Button title="Login" onPress={handleLogin} />
-        </View>
-    )
+import {firebaseConfig} from '../database/firebase'
+
+
+const RegisterScreen = ({navigation}) =>  {
+
+    const [email , setEmail] = useState('');
+    const [password, setPassword ] = useState('');
+    const [displayname , setDisplayname] = useState('');
+
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+    const createAccount = () => {
+        if(email === '' && password === '')
+        {
+            Alert.alert('Enter details to signup!')
+        }else{
+            createUserWithEmailAndPassword(auth,email,password,displayname)
+            .then((user) => {
+                console.log(
+                  "Registration Successful. Please Login to proceed"
+                );
+                Alert.alert("Registration Successful. Please Login to proceed")
+                console.log(user);
+                navigation.navigate('Login');
+            })
+            
+        }
+
+       
+    }
+ 
+
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Name"
+                    autoCapitalize="none"
+                    value={displayname}
+                    onChangeText={(val) => setDisplayname(val)}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    autoCapitalize="none"
+                    value={email}
+                    onChangeText={(val) => setEmail(val)}
+                />
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    placeholder="Password"
+                    secureTextEntry
+                    autoCapitalize="none"
+                    onChangeText={(val) => setPassword(val)}
+                />
+                <Button title="Signup"
+                    onPress={createAccount}
+                />
+            </View>
+        )
+    
 }
 
 const styles = StyleSheet.create({
@@ -49,4 +88,6 @@ const styles = StyleSheet.create({
     },
 });
 
-export default RegisterScreen
+
+export default RegisterScreen;
+
